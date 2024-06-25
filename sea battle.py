080e9e9ddcd981be041d41944f8120aa4a247e22
Antonio <antoshka2003@bk.ru>
2024-06-26 01:44:52 +0300

    dev-master: sea battle update

--- sea battle.py
+++ sea battle.py
@@ -8,11 +8,17 @@ class Notifications:
         print('Make your move!')
 
     @staticmethod
-    def orientation_message():
+    def orient_message():
         return 'What type of {0} point ship do you want?\nH/V: '
+
+    @staticmethod
+    def placement_message():
+        return 'Choose point, where you want to place your ship\nExample (X,Y): '
 
 
 class Ship:
+    message = Notifications()
+
     def __init__(self, length=1, orientation='H'):
         self.length = length
         self.side = orientation
@@ -26,12 +32,6 @@ class Player:
 
     def __init__(self):
         self.ships = []
-
-    def set_ships(self):
-        for _, length in zip(range(6), [3, 2, 2]):
-            self.ships.append(Ship(length, input(self.message.orientation_message().format(length))))
-        for i in range(3):
-            self.ships.append(Ship())
 
     def get_ship_info(self):
         for ship in self.ships:
@@ -50,17 +50,19 @@ class Board:
 
 
 class Dot:
+    used_dots = []
+    placed_ships = []
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.cord = (x, y)
 
     def __eq__(self, other):
-        if self.x == other.x and self.y == other.y:
+        if self.cord == other.cord:
             raise ValueError('This point is already placed')
-    def moving(self):
-        move = input()
 
+    def placing_ships(self):
+        if self.cord not in self.placed_ships:
+            self.placed_ships.append(self.cord)
 
 
 class Move:
@@ -73,12 +75,24 @@ class Move:
         print(move.split())  # TODO: DOT REFACT CLASS
 
 
-class Game(Player):
+class Game(Player, Board):
+    def init_ships(self):
+        for _, length in zip(range(3), [3, 2, 2]):
+            orientation = input(self.message.orient_message().format(length))
+            if orientation not in 'HV':
+                raise ValueError('Wrong orientation')
+            self.ships.append(Ship(length, orientation))
+        for i in range(3):
+            self.ships.append(Ship())
 
-    def setup_ships(self):
+    def place_ship(self):
         for ship in self.ships:
-
+            self.get_ally_map()
+            dot = input(self.message.placement_message())
+            Dot(*map(int, dot.split()))
 
 
 test = Board()
 test.get_ally_map()
+test_player = Game()
+test_player.init_ships()

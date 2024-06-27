7f3afd2a8e284d580cf4adb0404cc173747b09b0
Antonio <antoshka2003@bk.ru>
2024-06-27 17:33:25 +0300

    dev-master: sea battle final

--- sea battle.py
+++ sea battle.py
@@ -1,153 +1,268 @@
-from random import randint
-ally_board = [['O', 'O', 'O', 'O', 'O', 'O'],
-              ['O', 'O', 'O', 'O', 'O', 'O'],
-              ['O', 'O', 'O', 'O', 'O', 'O'],
-              ['O', 'O', 'O', 'O', 'O', 'O'],
-              ['O', 'O', 'O', 'O', 'O', 'O'],
-              ['O', 'O', 'O', 'O', 'O', 'O']]
-print(ally_board)
-access = False
+from random import randint, choice
+from time import sleep
 
 
-class Notifications:
-    @staticmethod
-    def side_decide_message():
-        return input('Left/Right : ').lower()
-
-    @staticmethod
-    def move_message():
-        print('Your turn, example: (X,Y)')
-        print('Make your move!')
-
-    @staticmethod
-    def orient_message():
-        return 'What type of {0} point ship do you want?\nH/V: '
-
-    @staticmethod
-    def placement_message():
-        return 'Choose point, where you want to place your ship\nExample (X,Y): '
+class BoardExcept(Exception):
+    pass
 
 
-class Ship:
-    message = Notifications()
+class WrongShip(BoardExcept):
+    pass
 
-    def __init__(self, length=1, orientation='H'):
-        self.length = length
-        self.side = orientation
 
+class BoardOutException(BoardExcept):
     def __str__(self):
-        return f'Ship {self.length} dot length. {self.side} orientation'
+        return 'Out of border!'
 
 
-class Player:
-    message = Notifications()
-
-    def __init__(self):
-        self.ships = []
-
-    def get_ship_info(self):
-        for ship in self.ships:
-            print(ship)
-
-
-class Board:
-    enemy_board = ally_board.copy()
-
-    @staticmethod
-    def get_ally_map():
-        print(' '*4, '-'*27)
-        print(' '*12, 'Your map')
-        print(' '*5,'|', *[f'{i} |' for i in range(1, 7)])
-        print()
-        for i, line in enumerate(ally_board):
-            print(f'{i+1} |   | ', end='')
-            print(*line, sep=' | ', end=' |\n')
+class BoardUsedException(BoardExcept):
+    def __str__(self):
+        return "You've already shot this cell!"
 
 
 class Dot:
-    used_dots = []
-    placed_ships = []
-    message = Notifications()
-
-    def __init__(self, x, y, curr_ship):
-        self.cord = (x - 1, y - 1)
-        self.x = x - 1
-        self.y = y - 1
-        self.ship = curr_ship
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     def __eq__(self, other):
-        if self.cord == other.cord:
-            raise ValueError('This point is already placed')
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return f'Dot({self.x}, {self.y})'
+
+
+class Ship:
+    def __init__(self, bow: Dot, length: int, vertical: bool):
+        self.bow = bow
+        self.length = length
+        self.vertical = vertical
+        self.lives = length
+
+    @property
+    def dots(self):
+        ship_dots = []
+        for i in range(self.length):
+            curr_x = self.bow.x
+            curr_y = self.bow.y
+            if self.vertical:
+                curr_y += i
+            else:
+                curr_x += i
+            ship_dots.append(Dot(curr_x, curr_y))
+        return ship_dots
+
+    def is_hit(self, dot) -> bool:
+        return dot in self.dots
+
+
+class Board:
+    def __init__(self, size: int, hid=False):
+        self.size = size
+        self.hid = hid
+        self.field = [['O'] * size for _ in range(size)]
+        self.busy = []
+        self.ships = []
+        self.last_hit = []
+        self.count_destr_ships = 0
 
     def __str__(self):
-        return f'{self.cord}'
+        res = '  | ' + ' | '.join(map(str, range(1, self.size + 1))) + ' |'
+        for i, row in enumerate(self.field):
+            res += f'\n{i + 1} | ' + ' | '.join(row) + ' |'
+        if self.hid:
+            res = res.replace('■', 'O')
+        return res
 
-    def check_side(self, coordinate):
-        side = self.message.side_decide_message()
-        if side == 'left':
-            if coordinate - self.ship.length >= 0:
-                global access
-                access = True
-                return
-            else:
-                # TODO: raise Error
-        else:
-            if coordinate + self.ship.length <= 6:
-                global access
-                access = True
-            else:
-                # TODO: raise Error
-    def placing_ships(self):
-        global access
-        access = False
-        print(f'Your {self.ship.length} ship placing')
-        if self.cord not in self.placed_ships:
-            while not access:
-                if self.ship.side == 'H':
-                    self.check_side(self.x)
+    def out(self, d: Dot) -> bool:
+        return not (0 <= d.x < self.size and 0 <= d.y < self.size)
+
+    def contour(self, ship, visible=False):
+        around = [(i, j) for i in range(-1, 2) for j in range(-1, 2)]
+        for dot in ship.dots:
+            for dx, dy in around:
+                curr_dot = Dot(dot.x + dx, dot.y + dy)
+                if not self.out(curr_dot) and curr_dot not in self.busy:
+                    if visible:
+                        self.field[curr_dot.x][curr_dot.y] = '.'
+                    self.busy.append(curr_dot)
+
+    def add_ship(self, ship):
+        for d in ship.dots:
+            if d in self.busy or self.out(d):
+                raise WrongShip()
+        for d in ship.dots:
+            self.field[d.x][d.y] = '■'
+            self.busy.append(d)
+        self.ships.append(ship)
+        self.contour(ship)
+
+    def shot(self, d: Dot) -> bool:
+        if d in self.busy:
+            raise BoardUsedException()
+        if self.out(d):
+            raise BoardOutException()
+
+        self.busy.append(d)
+
+        for ship in self.ships:
+            if ship.is_hit(d):
+                self.field[d.x][d.y] = 'X'
+                print('Hit!')
+                ship.lives -= 1
+                if ship.lives == 0:
+                    self.count_destr_ships += 1
+                    self.contour(ship, visible=True)
+                    print('Ship was destroyed!')
+                    self.last_hit = []
+                    return False
                 else:
-                    self.check_side(self.y)
+                    print('Ship is wounded!')
+                    self.last_hit.append(d)
+                    return True
+
+        self.field[d.x][d.y] = '.'
+        print('Miss!')
+        return False
+
+    def begin(self):
+        self.busy = []
+
+    def defeat(self):
+        return self.count_destr_ships == len(self.ships)
 
 
-            ally_board[self.cord[1]][self.cord[0]] = '☑'
-            print(f'cord 1 ={self.cord[1]+1} //// cord 0 ={self.cord[0]+1}')
-            self.placed_ships.append(self.cord)
-        else:
-            print('Already placed in this spot. TODO!')
+class Player:
+    def __init__(self, board: Board, enemy: Board):
+        self.board = board
+        self.enemy = enemy
+
+    def ask(self):
+        raise NotImplementedError()
+
+    def move(self) -> bool:
+        while True:
+            try:
+                target = self.ask()
+                repeat = self.enemy.shot(target)
+                sleep(1)
+                return repeat
+            except BoardExcept as exception_:
+                print(exception_)
 
 
-class Move:
-    def __init__(self):
-        self.messages = Notifications
-
-    def make_move(self):
-        self.messages.move_message()
-        move = input()  # TODO: DOT CLASS
-        print(move.split())  # TODO: DOT REFACT CLASS
-
-
-class Game(Player, Board, Dot):
-    def init_ships(self):
-        for _, length in zip(range(3), [3, 2, 2]):
-            orientation = input(self.message.orient_message().format(length))
-            if orientation not in 'HV':
-                raise ValueError('Wrong orientation')
-            self.ships.append(Ship(length, orientation))
-        for i in range(3):
-            self.ships.append(Ship())
-
-    def place_ship(self):
-        access = False
-        while not access:
-            for ship in self.ships:
-                self.get_ally_map()
-                dot = input(self.message.placement_message())
-                point = Dot(*map(int, dot.split()), ship)
-                point.placing_ships()
+class AI(Player):
+    def ask(self) -> Dot:
+        last = self.enemy.last_hit
+        while True:
+            if last:
+                if len(last) == 1:
+                    near = ((0, 1), (0, -1), (1, 0), (-1, 0))
+                else:
+                    if last[0].x == last[-1].x:
+                        near = ((0, 1), (0, -1))
+                    else:
+                        near = ((1, 0), (-1, 0))
+                dx, dy = choice(near)
+                delt = choice((Dot(last[-1].x + dx, last[-1].y + dy), Dot(last[0].x + dx, last[0].y + dy)))
+            else:
+                delt = Dot(randint(0, 5), randint(0, 5))
+            if delt not in self.enemy.busy and not self.enemy.out(delt):
+                break
+        sleep(2)
+        print(f'Enemy hit this point: {delt.x + 1} {delt.y + 1}')
+        return delt
 
 
-test = Board()
-test.get_ally_map()
-test_player = Game()
-test_player.init_ships()
-test_player.place_ship()
+class User(Player):
+    def ask(self) -> Dot:
+        while True:
+            coord = input('Enter your shot:\t').split()
+            if len(coord) != 2:
+                print('Example : (X,Y)')
+                continue
+            x, y = coord
+            if not all((x.isdigit(), y.isdigit())):
+                print('Coordinates must be digits')
+                continue
+            return Dot(int(y) - 1, int(x) - 1)
+
+
+class Game:
+    def __init__(self, size=6):
+        self.lens = (3, 2, 2, 1, 1, 1, 1)
+        self.size = size
+        ai_board = self.random_board()
+        user_board = self.random_board()
+        ai_board.hid = True
+
+        self.ai = AI(ai_board, user_board)
+        self.pl = User(user_board, ai_board)
+
+    def generating_ally_field(self):
+        attempts = 0
+        board = Board(size=self.size)
+        for l in self.lens:
+            while True:
+                attempts += 1
+                if attempts > 2000:
+                    return None
+                ship = Ship(Dot(randint(0, self.size), (randint(0, self.size))), l, bool(randint(0, 1)))
+                try:
+                    board.add_ship(ship)
+                    break
+                except WrongShip:
+                    pass
+        board.begin()
+        return board
+
+    def random_board(self):
+        board = None
+        while board is None:
+            board = self.generating_ally_field()
+        return board
+
+    @staticmethod
+    def greet():
+        print('-' * 18)
+        print(' Entry format: x y ')
+        print(' x - column  ')
+        print(' y - line ')
+        print('-'*18)
+
+    def display_boards(self):
+        print('-' * self.size * 10)
+        print('Your board:'.ljust((self.size + 1) * 4 - 1) + ' ' * self.size + 'Enemy Board:')
+        for s1, s2 in zip(self.pl.board.__str__().split('\n'), self.ai.board.__str__().split('\n')):
+            print(s1 + ' ' * self.size + s2)
+
+    def loop(self):
+        step = 0
+        while True:
+            self.display_boards()
+            if step % 2 == 0:
+                print('Your turn!')
+                repeat = self.pl.move()
+            else:
+                print('Enemy turn!')
+                repeat = self.ai.move()
+            if repeat:
+                step -= 1
+
+            if self.ai.board.defeat():
+                self.display_boards()
+                print('You won!')
+                break
+            if self.pl.board.defeat():
+                self.display_boards()
+                print('You lose!')
+                break
+            step += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
+
+
+g = Game()
+g.start()
